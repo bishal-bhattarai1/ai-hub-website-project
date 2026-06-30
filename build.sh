7a8b127c50c418c3a5650e63ee38112ddbecb77c
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-07-01 00:08:45 +0545

    change done

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); username = os.environ.get('DJANGO_SUPERUSER_USERNAME'); email = os.environ.get('DJANGO_SUPERUSER_EMAIL', ''); password = os.environ.get('DJANGO_SUPERUSER_PASSWORD'); username and password and not User.objects.filter(username=username).exists() and User.objects.create_superuser(username=username, email=email, password=password)"
